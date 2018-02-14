6a7a01f5c695de57f408578ce74e3ace154ca4f2
krattan406 <30876207+krattan406@users.noreply.github.com>
2018-02-14 13:01:46 -0800

    Add files via upload

---
+++ 2_Tomcat/cookbooks/tomcat-chef/recipes/default.rb
@@ -0,0 +1,61 @@
+package 'java-1.7.0-openjdk-devel'
+
+group 'tomcat'
+
+user 'tomcat' do
+	manage_home false
+	shell '/bin/nologin'
+	group 'tomcat'
+	home '/opt/tomcat'
+end
+
+
+remote_file 'apache-tomcat-8.5.20.tar.gz' do
+  source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz'
+end
+
+directory '/opt/tomcat' do
+	 mode '0700'
+	group 'tomcat'	
+end
+
+execute 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+
+
+execute "manage permissions conf" do
+  command "chgrp -R tomcat /opt/tomcat/conf && chmod g+r /opt/tomcat/conf/*"
+end
+
+
+
+directory '/opt/tomcat/conf' do
+	mode '0070'
+end
+
+
+execute 'chmod -R g+r /opt/tomcat/conf/*'
+
+execute 'chown some files: work, temp, logs' do
+  command 'chown -R tomcat /opt/tomcat/work /opt/tomcat/temp /opt/tomcat/logs'
+end
+
+
+template '/etc/systemd/system/tomcat.service' do
+	source 'tomcat.service.erb'
+end
+
+execute 'systemctl daemon-reload'
+
+service 'tomcat' do
+	action [:start, :enable]
+end
+
+
+
+
+
+
+
+
+
+
